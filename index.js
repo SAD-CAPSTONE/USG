4d7455ac1f69bf91686ab2ffdd57f20ee7f2b7a2
Unknown <imjanellealag@gmail.com>
2018-06-11 15:38:28 +0800

    Updated Index

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
-/**
- * The 'dotenv' module basically reads data from a .env file
- * and loads it to process.env
- * 
- * Refer to https://github.com/motdotla/dotenv for the official documentation
- */
 require('dotenv').config();
+const express = require('express');
+const fs = require('fs');
+const https = require('https');
+const path = require('path');
+const winston = require('winston');
+const passportSetup = require('./config/passport-setup');
 
-/**
- * This imports the ExpressJS module
- */
-var express = require('express');
-
-/**
- * Here we are creating a new ExpressJS app
- */
-var app = express();
-
-/**
- * We now pass the app instance to a custom module 'app' for bootstrapping
- * Refer to this link for what boostrapping means: https://stackoverflow.com/a/1254561
- */
+let app = express();
 require('./app')(app);
 
-/**
- * This tells the app instance to listen to a certain port for any requests
- */
-app.listen(app.get('port'), () => {
-    console.log(`ExpressJS server listening to port ${app.get('port')}`);
-});
+let httpsOptions = {
+  cert: fs.readFileSync(path.join(__dirname, 'ssl', 'server.crt')),
+  key: fs.readFileSync(path.join(__dirname, 'ssl', 'server.key')),
+}
+
+let server = https.createServer(httpsOptions, app)
+  .listen(app.get('port'), () => {
+      console.log(`ExpressJS server listening to port ${app.get('port')}`);
+  });
+
+// let server = app.listen(app.get('port'), () => {
+//     console.log(`ExpressJS server listening to port ${app.get('port')}`);
+// });
