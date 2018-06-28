eaf976017d4c9cfbda9b1d3d191374c641051c74
Unknown <imjanellealag@gmail.com>
2018-06-28 22:16:57 +0800

    Revert "Purchase Order"
    This reverts commit 3752089ce5e21dcca096a50d48dc0c4a8f66801f.

--- app/modules/admin-purchOrder/routes.js
+++ app/modules/admin-purchOrder/routes.js
@@ -47,81 +47,82 @@ router.get('/form', (req,res)=>{
 
 router.post('/submitForm',(req,res)=>{
 
-  var ponum = "1000";
-  var startProdListNo = 1000;
-  var product = req.body.product; //[prod1,prod2,prod3]
-  var quantity = req.body.quantity; // [quan1,quan2,quan3]
-  var counter = 0;
+  console.log(`Entering 1`);
 
-  db.beginTransaction(function(err){
-    if(err){
-      console.log(err);
+  // query the last list no
+  db.query(`Select * from tblPurchaseOrderList order by intPOrderlistno desc limit 1`,(err1,results1,fields1)=>{
+    if (err1) console.log(err1);
+
+    var product = req.body.product; //[prod1,prod2,prod3]
+    var quantity = req.body.quantity; // [quan1,quan2,quan3]
+    var counter = 0;
+    var startProdListNo = 1000;
+
+    console.log(`entering 2`);
+
+    // insert to purchase Order table
+    db.query(`Insert into tblPurchaseOrder (intPurchaseOrderNo, intSupplierID, intAdminID, strSpecialNote) values ("${req.body.POnum}","${req.body.supplier}", "1000", "${req.body.specialnote}")`, (err2,results2,fields2)=>{
+      if (err2) console.log(err2);
+    });
+
+    // get the PO List no
+    if (results1 == null || results1 == undefined || results1.length == 0 ){
+      startProdListNo = 1000;
     }else{
-      console.log('entering 1');
-      // Select last purchase order no
-      db.query(`Select * from tblPurchaseOrder order by intPurchaseOrderNo desc limit 1`,(err1,results1,fields1)=>{
-        if (err1){
-          db.rollback(function(){
-            console.log(err1);
-          })
-        }else{
-          if(results1 == undefined || results1 == null){
-
-          }else if(results1.length == 0){
-
-          }else{
-            ponum = parseInt(results1[0].intPurchaseOrderNo) + 1;
-          }
-          console.log('entering 2')
-          // Insert into purchase order
-          db.query(`Insert into tblPurchaseOrder (intPurchaseOrderNo, intSupplierID, intAdminID, strSpecialNote) values ("${ponum}","${req.body.supplier}", "1000", "${req.body.specialnote}")`,(err2,results2,fields2)=>{
-            if(err2){
-              db.rollback(function(){
-                console.log(err2);
-              })
-            }else{
-              console.log('entering 3')
-              // Get the last po list number
-              db.query(`Select * from tblPurchaseOrderList order by intPOrderlistno desc limit 1`,(err3,results3,fields3)=>{
-                if (err3){
-                  db.rollback(function(){
-                    console.log(err3);
-                  })
-                }else{
-                  if(results3 == undefined || results3 == null){
-
-                  }else if(results3.length == 0){
-
-                  }else{
-                    startProdListNo = parseInt(results3[0].intPOrderlistno) +1;
-                  }
-
-                  console.log('entering 4')
-                  // insert each to purchase order List
-                  async.eachSeries(product,function(data,callback){
-                    db.query(`Insert into tblPurchaseOrderList (intPOrderlistno, intPurchaseOrderNo,
-                      strProduct, intQuantity) values ("${startProdListNo}", "${req.body.POnum}", "${product[counter]}", "${quantity[counter]}")`, (err4,results4,fields4)=>{
-                       if (err4){
-                         db.rollback(function(){
-                           console.log(err4);
-                         })
-                       }else{
-                         startProdListNo++;
-                         counter++;
-                         callback();
-                       }
-                    });
-                  }, function(err, results){
-                    res.send("yes");
-                  });
-                }
-              });
-            }
-          });
-        }
-      });
+      startProdListNo = parseInt(results1[0].intPOrderlistno) + 1;
     }
-  })
+
+    console.log(`Entering 3`);
+
+    // insert each purchase order list in db
+      async.eachSeries(product,function(data,callback){
+        db.query(`Insert into tblPurchaseOrderList (intPOrderlistno, intPurchaseOrderNo,      strProduct, intQuantity) values ("${startProdListNo}", "${req.body.POnum}", "${product[counter]}", "${quantity[counter]}")`, (err3,results3,fields3)=>{
+           if (err3) console.log(err3);
+
+           counter++;
+           startProdListNo++;
+           callback();
+      });
+
+      }, function(err, results){
+        console.log("Puchase order done!");
+        res.send("yes");
+      });
+
+  });
+});
+
+router.post('/addSupplier', (req,res)=>{
+
+  var id = 0;
+  db.query(`Select * from tblUser Order by intUserID desc limit 1`, (err1,results1,fields1)=>{
+    if (err1) console.log(err1);
+
+    if(results1.length == 0 || results1 == 'null' || results1 == 'undefined'){
+      id = 1000;
+    }else{
+      id = parseInt(results1[0].intUserID) + 1;
+    }
+
+    // replace with transactions
+    db.query(`Insert into tblUser (intUserID,intUserTypeNo,strFname,strMname,strLname) values ("${id}","2","${req.body.fname}","${req.body.mname}","${req.body.lname}")`, (err2,results2,fields2)=>{
+      if (err2) console.log(err2);
+
+      db.query(`Insert into tblSupplier (intUserID,strBusinessName,strBusinessAddress,strSupplierPhone,strSupplierMobile) values ("${id}","${req.body.bname}","${req.body.address}","${req.body.phone}","${req.body.mobile}")`,(err3,results3,fields3)=>{
+        if (err3) console.log(err3);
+
+        db.query(`Select * from tblPurchaseOrder`, (err,results,fields)=>{
+          if (err) console.log(err);
+          db.query(`Select * from tblSupplier join tblUser on tblSupplier.intUserID = tblUser.intUserID where intStatus = 1`, (erra,resultsa,fieldsa)=>{
+            if(err) console.log(err);
+            res.render('admin-purchOrder/views/purchaseOrderForm',  {re: results, su: resultsa});
+          });
+        });
+
+      });
+    });
+
+  });
 
 
 });
