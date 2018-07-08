113377bf0b5e17778a19c843176df208ad545056
Jon Ervin <balmacedajonervin@gmail.com>
2018-07-08 23:46:55 +0800

    Order Confirmation + Most Popular Query

--- app/modules/cust-home/routes.js
+++ app/modules/cust-home/routes.js
@@ -5,14 +5,18 @@ const priceFormat = require('../cust-0extras/priceFormat');
 
 function popularProducts(req,res,next){
   /*Most Popular Products;
-  *(tblproductlist)*(tblproductbrand)*(tblproductinventory)*(tblproductreview)*/
-  db.query(`SELECT A.*, ROUND(AVG(Review.intStars),1)AS aveRating, COUNT(Review.intProductReviewNo)AS cntRating, COUNT(Review.strReview)AS cntReview FROM(
-    SELECT tblproductlist.*, Inv.intInventoryNo, Inv.intStatus As InvStatus, Inv.productPrice, Brand.strBrand FROM tblproductlist
-    INNER JOIN (SELECT * FROM tblproductbrand)Brand ON tblproductlist.intBrandNo= Brand.intBrandNo
-    INNER JOIN (SELECT * FROM tblproductinventory)Inv ON tblproductlist.intProductNo= Inv.intProductNo
-    WHERE Brand.intStatus= 1 GROUP BY tblproductlist.intProductNo)A
-    LEFT JOIN (SELECT * FROM tblproductreview)Review ON A.intProductNo = Review.intProductNo
-    GROUP BY A.intProductNo ORDER BY intProductNo LIMIT 10`, function (err,  results, fields) {
+  *(tblproductlist)*(tblproductbrand)*(tblproductinventory)*(tblorderdetails)*(tblproductreview)*/
+  db.query(`SELECT B.*, ROUND(AVG(Review.intStars),1)AS aveRating, COUNT(Review.intProductReviewNo)AS cntRating,
+    COUNT(Review.strReview)AS cntReview FROM(
+  	SELECT A.*, Orders.intOrderDetailsNo, COUNT(Orders.intOrderDetailsNo)AS OrderCNT FROM(
+		SELECT tblproductlist.*, Inv.intInventoryNo, Inv.intStatus As InvStatus, Inv.productPrice, Brand.strBrand FROM tblproductlist
+		INNER JOIN (SELECT * FROM tblproductbrand)Brand ON tblproductlist.intBrandNo= Brand.intBrandNo
+		INNER JOIN (SELECT * FROM tblproductinventory)Inv ON tblproductlist.intProductNo= Inv.intProductNo
+		WHERE Brand.intStatus= 1)A
+  	LEFT JOIN (SELECT * FROM tblorderdetails)Orders ON A.intInventoryNo= Orders.intInventoryNo
+  	GROUP BY A.intProductNo)B
+    LEFT JOIN (SELECT * FROM tblproductreview)Review ON B.intProductNo = Review.intProductNo
+    GROUP BY B.intProductNo ORDER BY OrderCNT DESC LIMIT 10`, function (err,  results, fields) {
     if (err) console.log(err);
     results.map( obj => obj.productPrice = priceFormat(obj.productPrice.toFixed(2)) );
     req.popularProducts= results;
