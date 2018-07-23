89132ed51e5067de37ebab6264d29a6d585ad69f
Unknown <imjanellealag@gmail.com>
2018-07-23 21:20:31 +0800

    Receive Delivery Fix

--- app/modules/admin-receiveDelivery/routes.js
+++ app/modules/admin-receiveDelivery/routes.js
@@ -4,8 +4,7 @@ var moment = require('moment');
 var async = require('async');
 
 router.get('/', (req,res)=>{
-  db.query(`Select * from tblreceiveorder join tblpurchaseorder on tblreceiveorder.intPurchaseOrderno
-join tblSupplier on tblPurchaseOrder.intSupplierID = tblSupplier.intUserID`, (err1,results1,fields1)=>{
+  db.query(`Select * from tblreceiveorder join tblpurchaseOrder on tblreceiveorder.intpurchaseorderno = tblpurchaseorder.intpurchaseorderno join tblSupplier on tblPurchaseOrder.intSupplierID = tblSupplier.intUserID`, (err1,results1,fields1)=>{
   if (err1) console.log(err1);
   res.render('admin-receiveDelivery/views/receiveDelivery', {re: results1, moment: moment});
 
@@ -25,23 +24,43 @@ router.get('/form', (req,res)=>{
   });
 });
 
+var purch_no = "";
 router.post('/findNo', (req,res)=>{
 
-  var number = "";
+
   if (req.body.o == null || req.body.o == " " || req.body.o == undefined || req.body.o == ""){
-    number = 0;
+    purch_no = 0;
   }else{
-    number = req.body.o;
+    purch_no = req.body.o;
   }
 
-  db.query(`Select * from tblPurchaseOrder where intPurchaseOrderNo = ${number} and intStatus = 0`, (err1,results1,fields1)=>{
+  db.query(`Select * from tblPurchaseOrder where tblPurchaseOrder.intPurchaseOrderNo = "${purch_no}" and intStatus = 0`, (err1,results1,fields1)=>{
     if (err1) console.log(err1);
 
-    if (results1 == undefined || results1 == null || results1.length == 0){
+    if (results1 == undefined || results1 == null){
       res.send("no");
-    }else{
+    }
+    else if(results1.length == 0){
+      res.send("no");
+    }
+    else{
       res.send("yes");
     }
+  });
+});
+
+router.get('/loadOrderList',(req,res)=>{
+  db.query(`Select * from tblPurchaseOrder join tblPurchaseOrderList on tblPurchaseOrder.intPurchaseOrderNo = tblPurchaseOrderList.intPurchaseOrderNo where tblPurchaseOrder.intPurchaseOrderNo = "${purch_no}" and intStatus = 0`,(err1,results1,fields1)=>{
+    if(err1) console.log(err1);
+    if (!err1) res.render('admin-receiveDelivery/views/productLoader', {re: results1});
+
+  });
+});
+
+router.get('/deliveryDetails',(req,res)=>{
+  db.query(`Select * from tblReceiveOrder join tblReceiveOrderlist on tblReceiveOrder.intReceiveOrderNo = tblReceiveOrderlist.intReceiveOrderNo where tblReceiveOrderlist.intReceiveOrderNo = "${req.query.delivery}"`,(err1,results1,fields1)=>{
+    if(err1) console.log(err1);
+    if (!err1) res.render('admin-receiveDelivery/views/deliveryDetails',{re: results1, delivery_no: req.query.delivery, moment:moment});
   });
 });
 
@@ -117,55 +136,15 @@ router.post('/newDeliveryRecord', (req,res)=>{
                         }
                       })
                     }
-
                   });
                 }
               });
-
-
             }
           })
         }
       });
     }
   })
-
-  //
-  // // insert to receiveDelivery table
-  // db.query(`Insert into tblreceiveOrder (intReceiveOrderNo, intPurchaseOrderNo, intAdminID,   specialNotes) values ("${req.body.rno}", "${req.body.POno}", "1000", "${req.body.note}")`, (err1,results1,fields1)=>{
-  //   if (err1) console.log(err1);
-  // });
-  //
-  //   // query the last received delivery note
-  // db.query(`Select * from tblReceiveOrderlist order by intOrderReceivedNo desc limit 1`,(err2,results2,fields2)=>{
-  //     if (err2) console.log(err2);
-  //
-  //     if (results2 == null || results2 == undefined || results2.length == 0){
-  //       startNo = "1000";
-  //     }else{
-  //       startNo = parseInt(results2[0].intOrderReceivedNo)+1;
-  //     }
-  //
-  //
-  //
-  //     async.eachSeries(loop,function(data,callback){
-  //
-  //       var exdate = moment(req.body.expiration[counter]).format("YYYY-MM-DD");
-  //       console.log(exdate);
-  //       var d = "2001-02-31";
-  //       db.query(`INSERT INTO tblreceiveorderlist (intOrderReceivedNo, intReceiveOrderNo, strProduct, strSize, strConsignmentPrice, intQuantity, SRP, dateExpiration, intOrderStatus, strVariant) VALUES ("${startNo}", "${req.body.rno}", "${req.body.product[counter]}", "${req.body.size[counter]}", "", "${req.body.quantity[counter]}", "${req.body.srp[counter]}", "${exdate}", "${req.body.status[counter]}", "${req.body.variant[counter]}")`, (err3,results3,fields3)=>{
-  //         if (err3) console.log(err3);
-  //         counter++;
-  //         startNo++;
-  //         callback();
-  //       });
-  //   }, function(err,results){
-  //     if (err) console.log(err);
-  //     console.log("Receive Delivery Done!");
-  //   });
-  //
-  // });
-
 
 });
 
