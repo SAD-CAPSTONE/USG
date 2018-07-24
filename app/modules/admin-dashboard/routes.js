2ec02cc82e874aad91b97b25af5ef65d8a8c72c5
piolosales <pioloosales@gmail.com>
2018-07-25 02:34:10 +0800

    Task 5 Dashboard(75% Slight fix)INC Unique Visitors

--- app/modules/admin-dashboard/routes.js
+++ app/modules/admin-dashboard/routes.js
@@ -2,7 +2,7 @@ var router = require('express').Router();
 var db = require('../../lib/database')();
 //dashboard--
 router.get('/', (req,res)=>{
-    db.query(`SELECT COUNT(intOrderNo) cnt FROM tblorder WHERE intStatus = 1 UNION SELECT COUNT(intUserID) FROM tbluser WHERE intUserTypeNo = 3`,(err1,results1)=>{
+    db.query(`SELECT COUNT(intOrderNo) cnt FROM tblorder WHERE intStatus = 1 UNION SELECT COUNT(intUserID) FROM tblcustomer WHERE intStatus = 1`,(err1,results1)=>{
       if (err1) console.log(err1)
       console.log(results1)
       res.render('admin-dashboard/views/dashboard', {re1: results1[0].cnt, re2: results1[1].cnt, name: "name"});
