6e9d4f4182dd7854e505fa5c63f2e37f5534a4cd
piolosales <pioloosales@gmail.com>
2018-07-25 02:19:27 +0800

    Task 5 Dashboard(75%)INC Unique Visitors

--- app/modules/admin-dashboard/routes.js
+++ app/modules/admin-dashboard/routes.js
@@ -1,8 +1,14 @@
 var router = require('express').Router();
-
+var db = require('../../lib/database')();
+//dashboard--
 router.get('/', (req,res)=>{
-  res.render('admin-dashboard/views/dashboard', {name: "name"});
-})
+    db.query(`SELECT COUNT(intOrderNo) cnt FROM tblorder WHERE intStatus = 1 UNION SELECT COUNT(intUserID) FROM tbluser WHERE intUserTypeNo = 3`,(err1,results1)=>{
+      if (err1) console.log(err1)
+      console.log(results1)
+      res.render('admin-dashboard/views/dashboard', {re1: results1[0].cnt, re2: results1[1].cnt, name: "name"});
+    })
+  });
+
 
 router.get('/load',(req,res)=>{
   res.render('admin-dashboard/views/loading', {name: "name"});
