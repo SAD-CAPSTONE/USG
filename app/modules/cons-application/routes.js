cb052f10d70ad06af70e80ec2faedd919ea7fd16
Jon Ervin <balmacedajonervin@gmail.com>
2018-08-28 00:25:43 +0800

    Consignor Registration Validations pt2

--- app/modules/cons-application/routes.js
+++ app/modules/cons-application/routes.js
@@ -30,11 +30,30 @@ router.post('/test',(req,res)=>{
   res.send("yes");
 });
 
-
 router.post('/register',(req,res)=>{
   var user_no = "1000";
   var contract_no = "1000";
   // change array to string
+
+  let cert = req.body.certification ?
+    req.body.certification.reduce((str, data)=>{
+      str ? str += `, ` : 0;
+      return str += data;
+    },'') : null,
+  mkt = req.body.marketing ? 1 : 0,
+  cat = req.body.category ?
+    req.body.category.reduce((str, data)=>{
+      str ? str += `, ` : 0;
+      return str += data;
+    },'') : null,
+  phone = req.body.phone.slice(4).slice(0,3) + req.body.phone.slice(4).slice(4),
+  mname = req.body.mname ? req.body.mname : null,
+  fda = req.body.fda ? req.body.fda : null,
+  besttime = req.body.besttime ? req.body.besttime : null;
+
+  cat ?
+    req.body.others && req.body.specify ? cat += `, ${req.body.specify}` : 0
+  : 0
 
   db.beginTransaction(function(err){
     if(err){db.rollback(function(){console.log(err)})
@@ -44,11 +63,7 @@ router.post('/register',(req,res)=>{
       db.query(`Select * from tblUser order by intUserID desc limit 1`,(err1,user,fields1)=>{
         if(err1){db.rollback(function(){console.log(err1)})
         }else{
-          if(user == null || user == undefined){} else if(user.length == 0){}
-          else{
-            user_no = parseInt(user[0].intUserID) + 1;
-          }
-
+          user[0] ? user_no = parseInt(user[0].intUserID) + 1 : 0
 
           // Select last contract no
           db.query(`Select * from tblContract order by intContractNo desc limit 1`,(err2,contract,fields2)=>{
@@ -59,21 +74,29 @@ router.post('/register',(req,res)=>{
               else{ contract_no = parseInt(contract[0].intContractNo) + 1; }
 
                 // Insert to supplier record
-                db.query(`Insert into tblUser (intUserID, intUserTypeNo, strFName, strMName, strLName, strEmail) values ("${user_no}", 2, "${req.body.fname}", "${req.body.mname}", "${req.body.lname}", "${req.body.email}")`, (err3,useri,fields3)=>{
+                db.query(`Insert into tblUser (intUserID, intUserTypeNo, strFName, strMName, strLName, strEmail)
+                values (?,2,?,?,?,?)`,
+                [user_no, req.body.fname, req.body.mname, req.body.lname, req.body.email], (err3,useri,fields3)=>{
 
                   if (err3){db.rollback(function(){console.log(err3); res.send("error")})}
                   else{
 
-                    db.query(`Insert into tblSupplier (intUserID, intBusinessTypeNo, strBrands, strBusinessName, strBusinessEmail, strBusinessAddress, strSupplierPhone, strSupplierMobile, strBusinessTIN,  intSupplierType, intStatus) values ("${user_no}","${req.body.businessType}","${req.body.brands}","${req.body.businessname}", "${req.body.email}", "${req.body.businessaddress}", "${req.body.phone.slice(4)}","${req.body.mobile}","${req.body.businesstin}",  ${req.body.suppliertype}, 3)`,(err4,supplieri,fields4)=>{
+                    db.query(`Insert into tblSupplier (intUserID, intBusinessTypeNo, strBrands, strBusinessName, strBusinessEmail,
+                      strBusinessAddress, strSupplierPhone, strSupplierMobile, strBusinessTIN, intSupplierType, intStatus)
+                      values (?,?,?,?,?,?,?,?,?,?,3)`,
+                      [user_no, req.body.businessType, req.body.brands, req.body.businessname, req.body.email, req.body.businessaddress,
+                      phone, req.body.mobile, req.body.businesstin, req.body.suppliertype],(err4,supplieri,fields4)=>{
+
                       if(err4) {db.rollback(function(){console.log(err4); res.send("error")})}
                       else{
                         // Insert to contract record
-                        var cert = req.body.certification.toString();
-                        //var cert = "yes,yes";
-                        var cat = req.body.category.toString();
-                        //var cat = "yes,yes";
-                        db.query(`Insert into tblContract (intContractNo, intConsignorID, timeDayToReach, consignmentPrice, deliverySchedule, strFrequencyOfDelivery, intDTIStat, intBIRStat, intFDAStat, strProductCertifications, strCategories, strFDAID, replacementAgreement, marketingAgreement, intContractStatus) values ("${contract_no}", "${user_no}", "${req.body.besttime}", ${req.body.consignment}, "${req.body.deliverysched}", "${req.body.freq}",${req.body.dtistat},${req.body.birstat},${req.body.fdastat},"${cert}", "${cat}","${req.body.fda}",${req.body.replacement},
-                        ${req.body.marketing}, 0)`,(err5,contract1,fields5)=>{
+
+                        db.query(`Insert into tblContract (intContractNo, intConsignorID, timeDayToReach, consignmentPrice,
+                          deliverySchedule, strFrequencyOfDelivery, intDTIStat, intBIRStat, intFDAStat, strProductCertifications,
+                          strCategories, strFDAID, replacementAgreement, marketingAgreement, intContractStatus) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,0)`,
+                          [contract_no, user_no, besttime, req.body.consignment, req.body.deliverysched, req.body.freq,
+                          req.body.dtistat, req.body.birstat, req.body.fdastat, cert, cat, fda, req.body.replacement,
+                          mkt],(err5,contract1,fields5)=>{
                           if(err5) {db.rollback(function(){console.log(err5); res.send("error")})}
                           else{
                             db.commit(function(e){
