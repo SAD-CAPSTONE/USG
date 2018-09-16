b086d0397c3bc680c45fa5862a36d59b3f411780
Ralf Milan <otaku0146@yahoo.com>
2018-09-16 09:19:00 +0800

    Update

--- app/modules/cons-dashboard/routes.js
+++ app/modules/cons-dashboard/routes.js
@@ -1,15 +1,17 @@
 var router = require('express').Router();
 var db = require('../../lib/database')();
 var moment = require('moment');
+const userTypeAuth = require('../cust-0extras/userTypeAuth');
+const auth_cons = userTypeAuth.cons;
 
-router.get('/consignor-dash', (req,res)=>{
+router.get('/consignor-dash', auth_cons, (req,res)=>{
   db.query(`
     SELECT * from tblproductinventory join tblproductlist on tblproductinventory.intProductNo = tblproductlist.intproductno
     join tbluser on tbluser.intUserID = tblproductinventory.intUserID
     join tblsubcategory on tblsubcategory.intSubCategoryNo = tblproductlist.intSubCategoryNo
     join tblcategory on tblcategory.intcategoryno = tblsubcategory.intcategoryno
     join tblsupplier on tblsupplier.intUserID = tblproductinventory.intUserID
-    where intUserTypeNo = 2 AND intSupplierType = 2 AND tbluser.intUserID = 1001`,(err1,results1)=>{
+    where tbluser.intUserID = ${req.user.intUserID}`,(err1,results1)=>{
     if (err1) console.log(err1);
     res.render('cons-dashboard/views/cons-dashboard', {re: results1});
     console.log(results1);
@@ -20,21 +22,35 @@ router.get('/consignor-dash', (req,res)=>{
   //res.render('cons-dashboard/views/cons-products');
 //});
 
-router.get('/products', (req,res)=>{
+router.get('/products', auth_cons, (req,res)=>{
   db.query(`
     SELECT * from tblproductinventory join tblproductlist on tblproductinventory.intProductNo = tblproductlist.intproductno
     join tbluser on tbluser.intUserID = tblproductinventory.intUserID
     join tblsubcategory on tblsubcategory.intSubCategoryNo = tblproductlist.intSubCategoryNo
     join tblcategory on tblcategory.intcategoryno = tblsubcategory.intcategoryno
     join tblsupplier on tblsupplier.intUserID = tblproductinventory.intUserID
-    where intUserTypeNo = 2 AND intSupplierType = 2 AND tbluser.intUserID = 1001`,(err1,results1)=>{
+    where tbluser.intUserID = ${req.user.intUserID}`,(err1,results1)=>{
     if (err1) console.log(err1);
     res.render('cons-dashboard/views/cons-products', {re: results1});
   });
 });
 
-router.get('/orders', (req,res)=>{
+router.get('/orders', auth_cons, (req,res)=>{
   res.render('cons-dashboard/views/cons-orders');
+});
+
+router.get('/returns', auth_cons, (req,res)=>{
+  db.query(`
+    SELECT * from tblpurchaseorder join tblreceiveorder on tblpurchaseorder.intpurchaseorderno = tblreceiveorder.intpurchaseorderno
+    join tblreturnbadorders on tblreceiveorder.intreceiveorderno = tblreturnbadorders.intreceiveorderno
+    join tblbadorderslist on tblbadorderslist.intbadordersno = tblreturnbadorders.intbadordersno
+    join tblsupplier on tblsupplier.intuserid = tblpurchaseorder.intsupplierid
+    join tbluser on tbluser.intuserid = tblsupplier.intuserid
+    where tbluser.intUserID = ${req.user.intUserID}`,(err1,results1)=>{
+    if (err1) console.log(err1);
+    res.render('cons-dashboard/views/cons-returns', {re: results1});
+    console.log(results1);
+  });
 });
 
 exports.consignor = router;
