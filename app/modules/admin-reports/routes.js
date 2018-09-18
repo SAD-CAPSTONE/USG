dc0f3d91b664b93236c634dc2a80063cec0602ff
janellealag <imjanellealag@gmail.com>
2018-09-18 11:38:20 +0800

    Sales Report UI

--- app/modules/admin-reports/routes.js
+++ app/modules/admin-reports/routes.js
@@ -4,6 +4,10 @@ router.get('/', (req,res)=>{
   res.render('admin-reports/views/reports');
 });
 
+router.get('/sales',(req,res)=>{
+  res.render('admin-reports/views/sales');
+})
+
 // <%- include('../../../templates/admin-navbar.ejs') -%>
 
 exports.reports = router;
