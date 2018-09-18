78ee56e30cca04061df04358e3b60d0b2ba256c0
janellealag <imjanellealag@gmail.com>
2018-09-18 16:15:25 +0800

    Inventory Reports UI

--- app/modules/admin-reports/routes.js
+++ app/modules/admin-reports/routes.js
@@ -8,6 +8,10 @@ router.get('/sales',(req,res)=>{
   res.render('admin-reports/views/sales');
 })
 
+router.get('/inventory',(req,res)=>{
+  res.render('admin-reports/views/inventory');
+})
+
 // <%- include('../../../templates/admin-navbar.ejs') -%>
 
 exports.reports = router;
