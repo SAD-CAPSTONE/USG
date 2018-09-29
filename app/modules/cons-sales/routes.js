9ad50ab98f84bfe4e31a58a4712838b133e4e7d3
Jon Ervin <balmacedajonervin@gmail.com>
2018-09-29 19:56:10 +0800

    Consignor Sales Backend AJAX part 2

--- app/modules/cons-sales/routes.js
+++ app/modules/cons-sales/routes.js
@@ -5,6 +5,7 @@ const priceFormat = require('../cust-0extras/priceFormat');
 const userTypeAuth = require('../cust-0extras/userTypeAuth');
 const auth_cons = userTypeAuth.cons;
 const pageLimit = 10;
+const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
 function sizeString(obj){
   let curSize = ``;
@@ -64,25 +65,66 @@ router.post('/loadSales', monthsAvailable, yearsAvailable, (req,res)=>{
     prevSel: 'Previous Week',
     months: req.monthsAvailable,
     years: req.yearsAvailable,
+    monthSel: null,
+    yearSel: null,
     page: 1
   }
+
+  req.body.filterBy == 'specialDate' ?
+    req.monthsAvailable[0] ?
+      req.monthsAvailable.reduce((temp, month)=>{
+        console.log(temp)
+        return month.month == req.body.month ? 1 : temp
+      },0) ?
+        0 :
+        req.body.month == 'All' ?
+          0 :
+          config.filterBy = 'thisWeek'
+      : config.filterBy = 'thisWeek'
+    : 0
+
+  req.body.filterBy == 'specialDate' ?
+    req.yearsAvailable[0] ?
+      req.yearsAvailable.reduce((temp, year)=>{
+        console.log(temp)
+        return year.year == req.body.year ? 1 : temp
+      },0) ?
+        0 : config.filterBy = 'thisWeek'
+      : config.filterBy = 'thisWeek'
+    : 0
 
   switch (config.filterBy) {
     case 'thisWeek':
       sDate = 'WEEK(tblorderhistory.historyDate)= WEEK(CURDATE())';
+      sDatePrev = `${sDate}-1`;
       break;
     case 'thisMonth':
       sDate = 'MONTH(tblorderhistory.historyDate)= MONTH(CURDATE())';
+      sDatePrev = `${sDate}-1`;
       config.curSel = 'This Month';
       config.prevSel = 'Previous Month';
       break;
     case 'thisYear':
       sDate = 'YEAR(tblorderhistory.historyDate)= YEAR(CURDATE())';
+      sDatePrev = `${sDate}-1`;
       config.curSel = 'This Year';
       config.prevSel = 'Previous Year';
       break;
     default:
-      sDate = `MONTH(tblorderhistory.historyDate)= ${req.body.month} AND MONTH(tblorderhistory.historyDate)= ${req.body.year}`;
+      if(req.body.month == 'All'){
+        sDate = `YEAR(tblorderhistory.historyDate)= ${req.body.year}`;
+        sDatePrev = `${sDate}-1`;
+        config.curSel = `Year ${req.body.year}`;
+        config.prevSel = 'Previous Year';
+      }
+      else{
+        sDateMonth = `MONTH(tblorderhistory.historyDate)= ${req.body.month} `;
+        sDateYear = `AND YEAR(tblorderhistory.historyDate)= ${req.body.year}`;
+        sDate = `${sDateMonth} ${sDateYear}`;
+        sDatePrev = `${sDateMonth}-1 ${sDateYear}`;
+        config.curSel = `${monthNames[req.body.month-1]} ${req.body.year}`;
+        config.prevSel = 'Previous Month';
+      }
       break;
   }
 
@@ -107,10 +149,9 @@ router.post('/loadSales', monthsAvailable, yearsAvailable, (req,res)=>{
       INNER JOIN tblproductinventory ON tblproductlist.intProductNo= tblproductinventory.intProductNo
       INNER JOIN tblorderdetails ON tblproductinventory.intInventoryNo= tblorderdetails.intInventoryNo
       INNER JOIN tblorder ON tblorderdetails.intOrderNo= tblorder.intOrderNo INNER JOIN tblorderhistory ON tblorderhistory.intOrderNo= tblorder.intOrderNo
-      WHERE tblproductinventory.intUserID= ? AND tblorderhistory.intStatus= 3 AND ${sDate}-1`,
+      WHERE tblproductinventory.intUserID= ? AND tblorderhistory.intStatus= 3 AND ${sDatePrev}`,
       [req.user.intUserID], (err, salesCountPrevious, fields) => {
       if (err) console.log(err);
-      console.log(salesCountPrevious)
       if (salesCountPrevious[0].TotalPrice){
         salesCountPrevious[0].TotalPrice = priceFormat(salesCountPrevious[0].TotalPrice.toFixed(2));
       }
