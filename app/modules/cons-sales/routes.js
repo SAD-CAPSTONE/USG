7afb71b7ba150d3f3265236f9907b6806f441f5c
Jon Ervin <balmacedajonervin@gmail.com>
2018-09-30 00:33:40 +0800

    Consignor Sales Backend AJAX Complete
    * Recurrence Filter
    * Specific/Special Filter
    * Validations
    * Pagination

--- app/modules/cons-sales/routes.js
+++ app/modules/cons-sales/routes.js
@@ -4,7 +4,7 @@ const db = require('../../lib/database')();
 const priceFormat = require('../cust-0extras/priceFormat');
 const userTypeAuth = require('../cust-0extras/userTypeAuth');
 const auth_cons = userTypeAuth.cons;
-const pageLimit = 10;
+const pageLimit = 2;
 const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
 function sizeString(obj){
@@ -58,18 +58,16 @@ router.get('/', checkUser, auth_cons, (req,res)=>{
 });
 
 router.post('/loadSales', monthsAvailable, yearsAvailable, (req,res)=>{
-  let sCountCurrent, sCountPrevious, sProducts, sInv, sDate,
+  let sCountCurrent, sCountPrevious, sProducts, sInv, limitQuery,
   config = {
+    page: 1,
+    total_pages: 1,
     filterBy: req.body.filterBy,
     curSel: 'This Week',
     prevSel: 'Previous Week',
     months: req.monthsAvailable,
-    years: req.yearsAvailable,
-    monthSel: null,
-    yearSel: null,
-    page: 1
+    years: req.yearsAvailable
   }
-
   req.body.filterBy == 'specialDate' ?
     req.monthsAvailable[0] ?
       req.monthsAvailable.reduce((temp, month)=>{
@@ -82,7 +80,6 @@ router.post('/loadSales', monthsAvailable, yearsAvailable, (req,res)=>{
           config.filterBy = 'thisWeek'
       : config.filterBy = 'thisWeek'
     : 0
-
   req.body.filterBy == 'specialDate' ?
     req.yearsAvailable[0] ?
       req.yearsAvailable.reduce((temp, year)=>{
@@ -92,7 +89,6 @@ router.post('/loadSales', monthsAvailable, yearsAvailable, (req,res)=>{
         0 : config.filterBy = 'thisWeek'
       : config.filterBy = 'thisWeek'
     : 0
-
   switch (config.filterBy) {
     case 'thisWeek':
       sDate = 'WEEK(tblorderhistory.historyDate)= WEEK(CURDATE())';
@@ -127,6 +123,21 @@ router.post('/loadSales', monthsAvailable, yearsAvailable, (req,res)=>{
       }
       break;
   }
+  config.page = req.body.page ?
+    Number(req.body.page) ?
+      parseInt(req.body.page)
+      : 1
+    : 1
+
+  let productsQuery = `SELECT tblproductlist.intProductNo, tblproductbrand.strBrand, tblproductlist.strProductName, tblproductlist.strProductPicture, tblorderhistory.historyDate,
+    SUM(tblorderdetails.intQuantity)QtySold, SUM(purchasePrice*tblorderdetails.intQuantity)TotalPrice FROM tblproductlist
+    INNER JOIN tblproductbrand ON tblproductlist.intBrandNo= tblproductbrand.intBrandNo
+    INNER JOIN tblproductinventory ON tblproductlist.intProductNo= tblproductinventory.intProductNo
+    INNER JOIN tblorderdetails ON tblproductinventory.intInventoryNo= tblorderdetails.intInventoryNo
+    INNER JOIN tblorder ON tblorderdetails.intOrderNo= tblorder.intOrderNo
+    INNER JOIN tblorderhistory ON tblorderhistory.intOrderNo= tblorder.intOrderNo
+    WHERE tblproductinventory.intUserID= ? AND tblorderhistory.intStatus= 3 AND ${sDate}
+    GROUP BY tblproductlist.intProductNo ORDER BY tblproductlist.intProductNo `
 
   fsalesCountCurrent();
   function fsalesCountCurrent(){
@@ -135,12 +146,12 @@ router.post('/loadSales', monthsAvailable, yearsAvailable, (req,res)=>{
       INNER JOIN tblorderdetails ON tblproductinventory.intInventoryNo= tblorderdetails.intInventoryNo
       INNER JOIN tblorder ON tblorderdetails.intOrderNo= tblorder.intOrderNo INNER JOIN tblorderhistory ON tblorderhistory.intOrderNo= tblorder.intOrderNo
       WHERE tblproductinventory.intUserID= ? AND tblorderhistory.intStatus= 3 AND ${sDate}`,
-      [req.user.intUserID], (err, salesCountCurrent, fields) => {
+      [req.user.intUserID], (err, results, fields) => {
       if (err) console.log(err);
-      if (salesCountCurrent[0].TotalPrice){
-        salesCountCurrent[0].TotalPrice = priceFormat(salesCountCurrent[0].TotalPrice.toFixed(2));
+      if (results[0]){
+        results[0].TotalPrice ? results[0].TotalPrice = priceFormat(results[0].TotalPrice.toFixed(2)) : 0
       }
-      sCountCurrent = salesCountCurrent
+      sCountCurrent = results
       fsalesCountPrevious();
     });
   }
@@ -150,31 +161,53 @@ router.post('/loadSales', monthsAvailable, yearsAvailable, (req,res)=>{
       INNER JOIN tblorderdetails ON tblproductinventory.intInventoryNo= tblorderdetails.intInventoryNo
       INNER JOIN tblorder ON tblorderdetails.intOrderNo= tblorder.intOrderNo INNER JOIN tblorderhistory ON tblorderhistory.intOrderNo= tblorder.intOrderNo
       WHERE tblproductinventory.intUserID= ? AND tblorderhistory.intStatus= 3 AND ${sDatePrev}`,
-      [req.user.intUserID], (err, salesCountPrevious, fields) => {
+      [req.user.intUserID], (err, results, fields) => {
       if (err) console.log(err);
-      if (salesCountPrevious[0].TotalPrice){
-        salesCountPrevious[0].TotalPrice = priceFormat(salesCountPrevious[0].TotalPrice.toFixed(2));
+      if (results[0]){
+        results[0].TotalPrice ? results[0].TotalPrice = priceFormat(results[0].TotalPrice.toFixed(2)) : 0
       }
-      sCountPrevious = salesCountPrevious
-      fsalesProducts()
+      sCountPrevious = results
+      fsalesProdCount()
+    });
+  }
+  function fsalesProdCount(){
+    db.query(`SELECT COUNT(A.intProductNo)cnt FROM(${productsQuery})A`,
+      [req.user.intUserID], (err, results, fields) => {
+      if (err) console.log(err);
+      if(results[0]){
+        config.total_pages =
+          results[0].cnt >= pageLimit ?
+            results[0].cnt % pageLimit ?
+              Math.floor(results[0].cnt / pageLimit) + 1 :
+            Math.floor(results[0].cnt / pageLimit)
+          : 1
+        config.page =
+          config.page > config.total_pages ?
+            config.total_pages :
+            config.page < 1 ? 1 : config.page
+        config.page =
+          config.page > config.total_pages ?
+            config.total_pages :
+            config.page < 1 ?
+              1 : config.page
+      }
+      // limit
+      let start = 0;
+      for(let i=0; i<config.page-1; i++){
+        start += pageLimit;
+      }
+      limitQuery = productsQuery.concat(`LIMIT ${start},${pageLimit} `);
+      fsalesProducts();
     });
   }
   function fsalesProducts(){
-    db.query(`SELECT tblproductlist.intProductNo, tblproductbrand.strBrand, tblproductlist.strProductName, tblproductlist.strProductPicture, tblorderhistory.historyDate,
-      SUM(tblorderdetails.intQuantity)QtySold, SUM(purchasePrice*tblorderdetails.intQuantity)TotalPrice FROM tblproductlist
-      INNER JOIN tblproductbrand ON tblproductlist.intBrandNo= tblproductbrand.intBrandNo
-      INNER JOIN tblproductinventory ON tblproductlist.intProductNo= tblproductinventory.intProductNo
-      INNER JOIN tblorderdetails ON tblproductinventory.intInventoryNo= tblorderdetails.intInventoryNo
-      INNER JOIN tblorder ON tblorderdetails.intOrderNo= tblorder.intOrderNo
-      INNER JOIN tblorderhistory ON tblorderhistory.intOrderNo= tblorder.intOrderNo
-      WHERE tblproductinventory.intUserID= ? AND tblorderhistory.intStatus= 3 AND ${sDate}
-      GROUP BY tblproductlist.intProductNo ORDER BY tblproductlist.intProductNo`,
-      [req.user.intUserID], (err, salesProducts, fields) => {
+    db.query(limitQuery,
+      [req.user.intUserID], (err, results, fields) => {
       if (err) console.log(err);
-      salesProducts.forEach((obj)=>{
+      results.forEach((obj)=>{
         obj.TotalPrice = priceFormat(obj.TotalPrice.toFixed(2));
       });
-      sProducts = salesProducts
+      sProducts = results
       fsalesInv();
     });
   }
@@ -188,14 +221,14 @@ router.post('/loadSales', monthsAvailable, yearsAvailable, (req,res)=>{
       INNER JOIN tblorderhistory ON tblorderhistory.intOrderNo= tblorder.intOrderNo
       WHERE tblproductinventory.intUserID= ? AND tblorderhistory.intStatus= 3 AND ${sDate}
       GROUP BY tblproductinventory.intInventoryNo ORDER BY tblproductlist.intProductNo`,
-      [req.user.intUserID], (err, salesInv, fields) => {
+      [req.user.intUserID], (err, results, fields) => {
       if (err) console.log(err);
-      salesInv.forEach((obj)=>{
+      results.forEach((obj)=>{
         obj.curSize = sizeString(obj);
         obj.TotalPrice = priceFormat(obj.TotalPrice.toFixed(2));
         obj.purchasePrice = priceFormat(obj.purchasePrice.toFixed(2));
       });
-      sInv = salesInv
+      sInv = results
       res.send({
         salesCountCurrent: sCountCurrent,
         salesCountPrevious: sCountPrevious,
@@ -206,7 +239,91 @@ router.post('/loadSales', monthsAvailable, yearsAvailable, (req,res)=>{
     });
   }
 });
+router.post('/prodSalesDetails', monthsAvailable, yearsAvailable, (req,res)=>{
+  let sProducts, sInv,
+  config = {
+    filterBy: req.body.filterBy
+  }
+  req.body.filterBy == 'specialDate' ?
+    req.monthsAvailable[0] ?
+      req.monthsAvailable.reduce((temp, month)=>{
+        console.log(temp)
+        return month.month == req.body.month ? 1 : temp
+      },0) ?
+        0 :
+        req.body.month == 'All' ?
+          0 :
+          config.filterBy = 'thisWeek'
+      : config.filterBy = 'thisWeek'
+    : 0
+  req.body.filterBy == 'specialDate' ?
+    req.yearsAvailable[0] ?
+      req.yearsAvailable.reduce((temp, year)=>{
+        console.log(temp)
+        return year.year == req.body.year ? 1 : temp
+      },0) ?
+        0 : config.filterBy = 'thisWeek'
+      : config.filterBy = 'thisWeek'
+    : 0
+  switch (config.filterBy) {
+    case 'thisWeek':
+      sDate = 'WEEK(tblorderhistory.historyDate)= WEEK(CURDATE())';
+      break;
+    case 'thisMonth':
+      sDate = 'MONTH(tblorderhistory.historyDate)= MONTH(CURDATE())';
+      break;
+    case 'thisYear':
+      sDate = 'YEAR(tblorderhistory.historyDate)= YEAR(CURDATE())';
+      break;
+    default:
+      if(req.body.month == 'All'){
+        sDate = `YEAR(tblorderhistory.historyDate)= ${req.body.year}`;
+      }
+      else{
+        sDateMonth = `MONTH(tblorderhistory.historyDate)= ${req.body.month} AND YEAR(tblorderhistory.historyDate)= ${req.body.year}`;
+      }
+      break;
+  }
 
+  db.query(`SELECT tblproductlist.intProductNo, tblproductbrand.strBrand, tblproductlist.strProductName, tblproductlist.strProductPicture, tblorderhistory.historyDate,
+    SUM(tblorderdetails.intQuantity)QtySold, SUM(purchasePrice*tblorderdetails.intQuantity)TotalPrice FROM tblproductlist
+    INNER JOIN tblproductbrand ON tblproductlist.intBrandNo= tblproductbrand.intBrandNo
+    INNER JOIN tblproductinventory ON tblproductlist.intProductNo= tblproductinventory.intProductNo
+    INNER JOIN tblorderdetails ON tblproductinventory.intInventoryNo= tblorderdetails.intInventoryNo
+    INNER JOIN tblorder ON tblorderdetails.intOrderNo= tblorder.intOrderNo
+    INNER JOIN tblorderhistory ON tblorderhistory.intOrderNo= tblorder.intOrderNo
+    WHERE tblproductinventory.intUserID= ? AND tblproductlist.intProductNo= ? AND tblorderhistory.intStatus= 3 AND ${sDate}
+    GROUP BY tblproductlist.intProductNo ORDER BY tblproductlist.intProductNo`,
+    [req.user.intUserID, req.body.prodid], (err, results, fields) => {
+    if (err) console.log(err);
+    results.forEach((obj)=>{
+      obj.TotalPrice = priceFormat(obj.TotalPrice.toFixed(2));
+    });
+    sProducts = results
+    db.query(`SELECT tblproductlist.intProductNo, tblproductinventory.strVariant, tblproductinventory.intSize, tbluom.strUnitName, tblorderdetails.purchasePrice,
+      tblorderhistory.historyDate, SUM(tblorderdetails.intQuantity)QtySold, SUM(purchasePrice*tblorderdetails.intQuantity)TotalPrice FROM tblproductlist
+      INNER JOIN tblproductinventory ON tblproductlist.intProductNo= tblproductinventory.intProductNo
+      INNER JOIN tbluom ON tblproductinventory.intUOMno= tbluom.intUOMno
+      INNER JOIN tblorderdetails ON tblproductinventory.intInventoryNo= tblorderdetails.intInventoryNo
+      INNER JOIN tblorder ON tblorderdetails.intOrderNo= tblorder.intOrderNo
+      INNER JOIN tblorderhistory ON tblorderhistory.intOrderNo= tblorder.intOrderNo
+      WHERE tblproductinventory.intUserID= ? AND tblproductlist.intProductNo= ? AND tblorderhistory.intStatus= 3 AND ${sDate}
+      GROUP BY tblproductinventory.intInventoryNo ORDER BY tblproductlist.intProductNo`,
+      [req.user.intUserID, req.body.prodid], (err, results, fields) => {
+      if (err) console.log(err);
+      results.forEach((obj)=>{
+        obj.curSize = sizeString(obj);
+        obj.TotalPrice = priceFormat(obj.TotalPrice.toFixed(2));
+        obj.purchasePrice = priceFormat(obj.purchasePrice.toFixed(2));
+      });
+      sInv = results
+      res.send({
+        product: sProducts,
+        inv: sInv,
+        config: config
+      });
+    });
+  });
+});
 
-// exports.consignor = router;
 exports.consignorSales = router;
