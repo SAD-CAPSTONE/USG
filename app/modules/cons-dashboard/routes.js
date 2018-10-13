546cebb78f04bdebca811f92d9c7fb7d7e0157ac
Unknown <imjanellealag@gmail.com>
2018-10-13 08:15:10 +0800

    Consignor module updates

--- app/modules/cons-dashboard/routes.js
+++ app/modules/cons-dashboard/routes.js
@@ -18,9 +18,6 @@ router.get('/consignor-dash', auth_cons, (req,res)=>{
   });
 });
 
-//router.get('/consignor-products', (req,res)=>{
-  //res.render('cons-dashboard/views/cons-products');
-//});
 
 router.get('/products', auth_cons, (req,res)=>{
   db.query(`
@@ -36,18 +33,62 @@ router.get('/products', auth_cons, (req,res)=>{
 });
 
 router.get('/orders', auth_cons, (req,res)=>{
-  db.query(`
-  SELECT * from tblpurchaseorderlist join tblreceiveorder on tblpurchaseorderlist.intpurchaseorderno = tblreceiveorder.intpurchaseorderno
-  join tblpurchaseorder on tblpurchaseorderlist.intPurchaseOrderNo = tblpurchaseorder.intPurchaseOrderNo
-  join tblreceiveorderlist on tblreceiveorderlist.intReceiveOrderNo = tblreceiveorder.intReceiveOrderNo
-  join tblsupplier on tblsupplier.intuserid = tblpurchaseorder.intsupplierid
-  join tbluser on tbluser.intuserid = tblsupplier.intuserid
-  where tbluser.intUserID = ${req.user.intUserID}`,(err1,results1)=>{
+  // Pending Orders
+  db.query(` Select * from tblpurchaseorder where intsupplierid
+   = ${req.user.intUserID} and intStatus = 0`,(err1,results1,fie1)=>{
   if (err1) console.log(err1);
-  res.render('cons-dashboard/views/cons-orders', {re: results1});
-  console.log(results1);
+  else{
+
+        res.render('cons-dashboard/views/cons-orders', {pending: results1, moment: moment});
+
+
+  }
+
 });
 });
+
+router.get('/viewOrder',(req,res)=>{
+  db.query(`Select * from tblpurchaseorder join tblPurchaseOrderList on tblPurchaseOrder.intpurchaseorderno = tblPurchaseOrderList.intPurchaseOrderNo
+    where tblPurchaseOrder.intPurchaseOrderNo = "${req.query.order}" and intsupplierid = "${req.user.intUserID}"`,(err1,res1,fie1)=>{
+      if(err1) console.log(err1);
+      else{
+        db.query(`Select * from tblUser join tblsupplier on tblUser.intUserID = tblsupplier.intUserID
+          where tblUser.intUserID = "${req.user.intUserID}"`,(err2,res2,fie2)=>{
+            if(err2) console.log(err2);
+            else{
+              res.render('cons-dashboard/views/viewOrder', {re: res1, consignor: res2, moment: moment});
+            }
+          })
+      }
+    })
+})
+
+router.get('/receivedOrders',(req,res)=>{
+  db.query(`Select * from tblpurchaseorder join tblreceiveorder on tblpurchaseorder.intPurchaseOrderNo
+  = tblreceiveorder.intPurchaseOrderNo where intsupplierid = "${req.user.intUserID}"`,(err2,res2,fie2)=>{
+    if(err2) console.log(err2)
+    else{
+      res.render('cons-dashboard/views/cons-receivedOrders', {received: res2, moment: moment});
+
+    }
+  })
+});
+
+router.get('/viewReceivedOrder',(req,res)=>{
+  db.query(`Select * from tblreceiveorder join tblreceiveorderlist on tblreceiveorder.intReceiveOrderNo
+    = tblreceiveorderlist.intReceiveOrderNo where tblreceiveorder.intReceiveOrderNo = "${req.query.order}"`,(err1,res1,fie1)=>{
+      if(err1) console.log(err1);
+      else{
+        db.query(`Select * from tblUser join tblSupplier on tblUser.intUserID = tblSupplier.intUserID
+          where tblUser.intUserID = "${req.user.intUserID}"`,(err2,res2,fie2)=>{
+            if(err2) console.log(err2);
+            else{
+              res.render('cons-dashboard/views/viewReceivedOrder',{re: res1, moment: moment, consignor: res2})
+            }
+          })
+      }
+    })
+})
 
 router.get('/returns', auth_cons, (req,res)=>{
   db.query(`
